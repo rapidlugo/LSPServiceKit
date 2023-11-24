88c5a839d2666485b7b61c43f9088b51396e122b
rapidlugo <119347329+rapidlugo@users.noreply.github.com>
2023-11-24 15:30:59 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "LSPServiceKit",
-    platforms: [.iOS(.v11), .tvOS(.v11), .macOS(.v13)],
+    platforms: [.iOS(.v13), .tvOS(.v13), .macOS(.v13)],
     products: [
         .library(
             name: "LSPServiceKit",
